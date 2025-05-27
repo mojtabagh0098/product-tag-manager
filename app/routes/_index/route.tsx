2b4ef679ffb16b5399ab546741412263271a16b1
mojtaba <mojtabagh0098@gmail.com>
2025-05-28 02:17:23 +0330

    add AppProvider

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,6 +1,6 @@
 import { useLoaderData } from "@remix-run/react";
 import { json } from "@remix-run/node";
-import { Card, Page } from "@shopify/polaris";
+import { AppProvider, Card, Page } from "@shopify/polaris";
 import prisma from "../../db.server";
 
 type ProductTag = {
@@ -18,13 +18,15 @@ export default function Index() {
   const { tags } = useLoaderData<typeof loader>();
 
   return (
-    <Page title="Product Tags Manager">
-      {tags.map((tag: ProductTag) => (
-        <Card key={tag.id} padding="400" roundedAbove="sm">
-          <h2>Product ID: {tag.productId}</h2>
-          <p>{tag.tags.join(", ")}</p>
-        </Card>
-      ))}
-    </Page>
+    <AppProvider>
+        <Page title="Product Tags Manager">
+        {tags.map((tag: ProductTag) => (
+            <Card key={tag.id} padding="400" roundedAbove="sm">
+            <h2>Product ID: {tag.productId}</h2>
+            <p>{tag.tags.join(", ")}</p>
+            </Card>
+        ))}
+        </Page>
+    </AppProvider>
   );
 }
