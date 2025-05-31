169841a63b8c320866dd98cbc9f1cc01f9430773
mojtaba <mojtabagh0098@gmail.com>
2025-06-01 01:34:09 +0330

    fix 6

--- app/routes/products.tsx
+++ app/routes/products.tsx
@@ -9,19 +9,19 @@ import { authenticate } from "../shopify.server";
 import prisma from "../db.server";
 import {
   Page,
-//   Card,
-//   InlineStack,
-//   Text,
-//   TextField,
-//   Button,
-//   Box,
-//   Icon,
+  Card,
+  InlineStack,
+  Text,
+  TextField,
+  Button,
+  Box,
+  Icon,
 } from "@shopify/polaris";
-// import { useLoaderData, useFetcher } from "@remix-run/react";
-// import { useState } from "react";
-// import {
-//   XSmallIcon
-// } from '@shopify/polaris-icons';
+import { useLoaderData, useFetcher } from "@remix-run/react";
+import { useState } from "react";
+import {
+  XSmallIcon
+} from '@shopify/polaris-icons';
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
     const { admin, session } = await authenticate.admin(request);
@@ -87,7 +87,7 @@ export const action = async ({ request }: ActionFunctionArgs) => {
 };
 
 export default function ProductList() {
-//   const { products, tags } = useLoaderData<typeof loader>();
+  const { products, tags } = useLoaderData<typeof loader>();
 //   const fetcher = useFetcher();
 //   const [formStates, setFormStates] = useState<Record<string, string>>({});
 
@@ -97,7 +97,71 @@ export default function ProductList() {
 
   return (
     <Page>
-        products
+        {products.map((product: any) => {
+            const productTags = tags.find(
+            (t: any) => t.productId === product.id
+            )?.tags || [];
+
+            return (
+            <Card key={product.id} padding="400">
+                <Box paddingBlockEnd="300">
+                <Text as="p" variant="bodySm" tone="subdued">
+                    Handle: {product.handle}
+                </Text>
+                </Box>
+
+                {/* نمایش تگ‌ها */}
+                {productTags.length > 0 && (
+                <InlineStack gap="200" wrap>
+                    {/* {productTags.map((tag: string, index: number) => (
+                    <fetcher.Form method="post" key={index}>
+                        <input
+                        type="hidden"
+                        name="productId"
+                        value={product.id}
+                        />
+                        <input type="hidden" name="removeTag" value={tag} />
+                        <Box
+                        paddingInline="300"
+                        paddingBlock="200"
+                        background="bg-surface-secondary"
+                        borderRadius="200"
+                        >
+                        <Text as="span" variant="bodySm" tone="subdued">
+                            #{tag}
+                        </Text>
+                        <Button
+                            icon={<Icon source={XSmallIcon} tone="base" />}
+                            accessibilityLabel={`Remove ${tag}`}
+                            size="micro"
+                            variant="plain"
+                        />
+                        </Box>
+                    // </fetcher.Form>
+                    ))} */}
+                </InlineStack>
+                )}
+                {/* <fetcher.Form method="post">
+                <input type="hidden" name="productId" value={product.id} />
+                <InlineStack gap="300" align="center">
+                    <Box maxWidth="300px">
+                    <TextField
+                        label="Add tag"
+                        labelHidden
+                        name="tag"
+                        autoComplete="off"
+                        value={formStates[product.id] || ""}
+                        onChange={(value) => handleChange(product.id, value)}
+                    />
+                    </Box>
+                    <Button submit variant="primary">
+                    Add
+                    </Button>
+                </InlineStack>
+                </fetcher.Form> */}
+            </Card>
+            );
+        })}
     </Page>
   );
 }
